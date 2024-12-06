fc4a6e57e16ad8095a649712de3bdb82d4396306
Midas@tower <javier.liano@alumnos.uneatlantico.es>
2024-12-07 00:41:30 +0100

    not bad start

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -3,84 +3,38 @@ return {
   priority = 1000,
   lazy = false,
   ---@type snacks.Config
-opts = {
+  opts = {
     bigfile = { enabled = true },
     dashboard = {
-  sections = {
-    { section = "header" },
-    {
-      pane = 2,
-      section = "terminal",
-      cmd = "wsl colorscript -e square",
-      height = 5,
-      padding = 1,
-    },
-    { section = "keys", gap = 1, padding = 1 },
-    {
-      pane = 2,
-      icon = " ",
-      desc = "Browse Repo",
-      padding = 1,
-      key = "b",
-      action = function()
-        Snacks.gitbrowse()
-      end,
-    },
-    function()
-      local in_git = Snacks.git.get_root() ~= nil
-      local cmds = {
+      sections = {
+        { section = "header" },
         {
-          title = "Notifications",
-          cmd = "gh notify -s -a -n5",
-          action = function()
-            vim.ui.open("https://github.com/notifications")
-          end,
-          key = "n",
-          icon = " ",
-          height = 5,
-          enabled = true,
-        },
-        {
-          title = "Open Issues",
-          cmd = "gh issue list -L 3",
-          key = "i",
-          action = function()
-            vim.fn.jobstart("gh issue list --web", { detach = true })
-          end,
-          icon = " ",
-          height = 7,
-        },
-        {
-          icon = " ",
-          title = "Open PRs",
-          cmd = "gh pr list -L 3",
-          key = "p",
-          action = function()
-            vim.fn.jobstart("gh pr list --web", { detach = true })
-          end,
-          height = 7,
-        },
-        {
-          icon = " ",
-          title = "Git Status",
-          cmd = "hub --no-pager diff --stat -B -M -C",
-          height = 10,
-        },
-      }
-      return vim.tbl_map(function(cmd)
-        return vim.tbl_extend("force", {
           pane = 2,
           section = "terminal",
-          enabled = in_git,
+          cmd = "wsl colorscript -e square",
+          height = 5,
+          padding = 1,
+        },
+        { section = "keys", gap = 1, padding = 1 },
+        { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+        { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+        {
+          pane = 2,
+          icon = " ",
+          title = "Git Status",
+          section = "terminal",
+          enabled = function()
+            return Snacks.git.get_root() ~= nil
+          end,
+          cmd = "hub status --short --branch --renames",
+          height = 5,
           padding = 1,
           ttl = 5 * 60,
           indent = 3,
-        }, cmd)
-      end, cmds)
-    end,
-    { section = "startup" },
-  },
-},
+        },
+        { section = "startup" },
+      },
+    },
     notifier = {
       enabled = true,
       timeout = 3000,
@@ -90,26 +44,118 @@ opts = {
     words = { enabled = true },
     styles = {
       notification = {
-        wo = { wrap = true } -- Wrap notifications
-      }
-    }
+        wo = { wrap = true }, -- Wrap notifications
+      },
+    },
   },
   keys = {
-    { "<leader>.",  function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
-    { "<leader>S",  function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
-    { "<leader>n",  function() Snacks.notifier.show_history() end, desc = "Notification History" },
-    { "<leader>bd", function() Snacks.bufdelete() end, desc = "Delete Buffer" },
-    { "<leader>cR", function() Snacks.rename.rename_file() end, desc = "Rename File" },
-    { "<leader>gB", function() Snacks.gitbrowse() end, desc = "Git Browse" },
-    { "<leader>gb", function() Snacks.git.blame_line() end, desc = "Git Blame Line" },
-    { "<leader>gf", function() Snacks.lazygit.log_file() end, desc = "Lazygit Current File History" },
-    { "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" },
-    { "<leader>gl", function() Snacks.lazygit.log() end, desc = "Lazygit Log (cwd)" },
-    { "<leader>un", function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications" },
-    { "<c-/>",      function() Snacks.terminal() end, desc = "Toggle Terminal" },
-    { "<c-_>",      function() Snacks.terminal() end, desc = "which_key_ignore" },
-    { "]]",         function() Snacks.words.jump(vim.v.count1) end, desc = "Next Reference", mode = { "n", "t" } },
-    { "[[",         function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference", mode = { "n", "t" } },
+    {
+      "<leader>.",
+      function()
+        Snacks.scratch()
+      end,
+      desc = "Toggle Scratch Buffer",
+    },
+    {
+      "<leader>S",
+      function()
+        Snacks.scratch.select()
+      end,
+      desc = "Select Scratch Buffer",
+    },
+    {
+      "<leader>n",
+      function()
+        Snacks.notifier.show_history()
+      end,
+      desc = "Notification History",
+    },
+    {
+      "<leader>bd",
+      function()
+        Snacks.bufdelete()
+      end,
+      desc = "Delete Buffer",
+    },
+    {
+      "<leader>cR",
+      function()
+        Snacks.rename.rename_file()
+      end,
+      desc = "Rename File",
+    },
+    {
+      "<leader>gB",
+      function()
+        Snacks.gitbrowse()
+      end,
+      desc = "Git Browse",
+    },
+    {
+      "<leader>gb",
+      function()
+        Snacks.git.blame_line()
+      end,
+      desc = "Git Blame Line",
+    },
+    {
+      "<leader>gf",
+      function()
+        Snacks.lazygit.log_file()
+      end,
+      desc = "Lazygit Current File History",
+    },
+    {
+      "<leader>gg",
+      function()
+        Snacks.lazygit()
+      end,
+      desc = "Lazygit",
+    },
+    {
+      "<leader>gl",
+      function()
+        Snacks.lazygit.log()
+      end,
+      desc = "Lazygit Log (cwd)",
+    },
+    {
+      "<leader>un",
+      function()
+        Snacks.notifier.hide()
+      end,
+      desc = "Dismiss All Notifications",
+    },
+    {
+      "<c-/>",
+      function()
+        Snacks.terminal()
+      end,
+      desc = "Toggle Terminal",
+    },
+    {
+      "<c-_>",
+      function()
+        Snacks.terminal()
+      end,
+      desc = "which_key_ignore",
+    },
+    {
+      "]]",
+      function()
+        Snacks.words.jump(vim.v.count1)
+      end,
+      desc = "Next Reference",
+      mode = { "n", "t" },
+    },
+    {
+      "[[",
+      function()
+        Snacks.words.jump(-vim.v.count1)
+      end,
+      desc = "Prev Reference",
+      mode = { "n", "t" },
+    },
     {
       "<leader>N",
       desc = "Neovim News",
@@ -127,7 +173,7 @@ opts = {
           },
         })
       end,
-    }
+    },
   },
   init = function()
     vim.api.nvim_create_autocmd("User", {
@@ -148,7 +194,9 @@ opts = {
         Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
         Snacks.toggle.diagnostics():map("<leader>ud")
         Snacks.toggle.line_number():map("<leader>ul")
-        Snacks.toggle.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }):map("<leader>uc")
+        Snacks.toggle
+          .option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 })
+          :map("<leader>uc")
         Snacks.toggle.treesitter():map("<leader>uT")
         Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background" }):map("<leader>ub")
         Snacks.toggle.inlay_hints():map("<leader>uh")
